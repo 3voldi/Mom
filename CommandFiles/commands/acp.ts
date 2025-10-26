c45f0c12201c88ebdf1ae9447360bb3db3ffc889
3voldi <voldigoanos65@gmail.com>
2025-10-26 15:46:23 +0000

    Update acp.ts

--- CommandFiles/commands/acp.ts
+++ CommandFiles/commands/acp.ts
@@ -5,7 +5,7 @@ export const meta: CommandMeta = {
   name: "accept",
   otherNames: ["acp"],
   version: "2.0.0",
-  author: "JV Barcenas | Liane Cagara",
+  author: "Christus | Liane Cagara",
   role: 1.5,
   description: "Accepts friend requests.",
   category: "Utilities",
